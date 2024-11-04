0f792ac2b6134e100b43e660534cc9d7cffe0388
Chugaister <79446015+AndreyMaksimenko@users.noreply.github.com>
2024-11-04 10:20:25 +0200

    Update get_data_secure.sql

--- Laboratory-work-7/get_data_secure.sql
+++ Laboratory-work-7/get_data_secure.sql
@@ -2,6 +2,9 @@
 CREATE OR REPLACE FUNCTION get_data_secure(attr_value TEXT)
 RETURNS TABLE(s_id INTEGER, pupil_name VARCHAR, class VARCHAR, confidentiality_level INTEGER) AS $$
 BEGIN
-    RETURN QUERY SELECT * FROM pupil WHERE pupil_name = attr_value;
+    RETURN QUERY SELECT p.s_id, p.pupil_name, p.class, p.confidentiality_level 
+                 FROM pupil AS p 
+                 WHERE p.pupil_name = attr_value;
 END;
 $$ LANGUAGE plpgsql;
+
