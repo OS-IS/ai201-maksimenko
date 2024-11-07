ba46521513a7557a75c5121fea6dcb73225dadbd
Chugaister <79446015+AndreyMaksimenko@users.noreply.github.com>
2024-11-07 10:40:48 +0200

    Update get_data_secure.sql

--- Laboratory-work-7/get_data_secure.sql
+++ Laboratory-work-7/get_data_secure.sql
@@ -1,10 +1,9 @@
 -- get_data_secure.sql
-CREATE OR REPLACE FUNCTION get_data_secure(attr_value TEXT)
+CREATE OR REPLACE FUNCTION get_data(attr_value TEXT)
 RETURNS TABLE(s_id INTEGER, pupil_name VARCHAR, class VARCHAR, confidentiality_level INTEGER) AS $$
 BEGIN
-    RETURN QUERY SELECT p.s_id, p.pupil_name, p.class, p.confidentiality_level 
-                 FROM pupil AS p 
-                 WHERE p.pupil_name = attr_value;
+    RETURN QUERY EXECUTE 'SELECT * FROM pupil WHERE pupil_name = $1'
+    USING attr_value;
 END;
 $$ LANGUAGE plpgsql;
 
