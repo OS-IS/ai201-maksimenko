f0f501692d1a48c83f539b9145ff34a03708e518
Chugaister <79446015+AndreyMaksimenko@users.noreply.github.com>
2024-11-07 10:43:01 +0200

    Update get_data.sql

--- Laboratory-work-7/get_data.sql
+++ Laboratory-work-7/get_data.sql
@@ -1,8 +1,7 @@
--- get_data.sql
+--  get_data.sql
 CREATE OR REPLACE FUNCTION get_data(attr_value TEXT)
 RETURNS TABLE(s_id INTEGER, pupil_name VARCHAR, class VARCHAR, confidentiality_level INTEGER) AS $$
 BEGIN
-    RETURN QUERY EXECUTE 'SELECT * FROM pupil WHERE pupil_name = $1'
-    USING attr_value;
+    RETURN QUERY EXECUTE 'SELECT * FROM pupil WHERE pupil_name = ''' || attr_value || '''';
 END;
 $$ LANGUAGE plpgsql;
